fafe14af8e981c2ce4183c0229d24b3186506d93
Hazar Hascan <96652029+HzrHscn@users.noreply.github.com>
2023-12-18 21:25:01 +0300

    Hazar Hascan

---
+++ Controllers/FaturaController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCOnlineTicariOtomasyon.Models.Siniflar;
+
+namespace MVCOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        // GET: Fatura
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var liste = c.Faturalars.ToList();
+            return View(liste);
+        }
+
+        [HttpGet]
+        public ActionResult FaturaEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FaturaEkle(Faturalar p)
+        {
+            c.Faturalars.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult FaturaGetir(int id)
+        {
+            var fatura = c.Faturalars.Find(id);
+            return View("FaturaGetir", fatura);
+        }
+
+        public ActionResult FaturaGuncelle(Faturalar f)
+        {
+            var fatura = c.Faturalars.Find(f.Faturaid);
+            fatura.FaturaSeriNo = f.FaturaSeriNo;
+            fatura.FaturaSıraNo = f.FaturaSıraNo;
+            fatura.VergiDairesi = f.VergiDairesi;
+            fatura.Tarih= f.Tarih;
+            fatura.Saat= f.Saat;
+            fatura.TeslimEden= f.TeslimEden;
+            fatura.TeslimAlan= f.TeslimAlan;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult FaturaDetay(int id)
+        {
+            var degerler = c.FaturaKalems.Where(x => x.Faturaid == id).ToList();         
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKalem()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKalem(FaturaKalem p)
+        {
+            c.FaturaKalems.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Siniflar/Cariler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVCOnlineTicariOtomasyon.Models.Siniflar
+{
+    public class Cariler
+    {
+        [Key]
+        public int Cariid {get;set;}
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50,ErrorMessage ="En Fazla 50 Karakter Yazabilirsiniz!")]
+        public string CariAd { get;set;}
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        [Required(ErrorMessage ="Bu Alanı Boş Geçemezsiniz!")]
+        public string CariSoyad { get;set;}
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariSehir { get;set;}
+
+        [Column(TypeName = "Varchar")]
+        [StringLength(50)]
+        public string CariMail { get;set;}
+
+        public bool Durum {  get; set;}
+
+        public ICollection<SatisHareket> SatisHarekets { get; set; }
+    }
+}
